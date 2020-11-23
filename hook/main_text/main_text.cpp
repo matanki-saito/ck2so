3c6f699cfa6a0db5b3045800f924503864ba0b5b
matanki-saito <matanki.saito@gmail.com>
2020-11-23 19:46:14 +0900

    Sample code

--- hook/main_text/main_text.cpp
+++ hook/main_text/main_text.cpp
@@ -1,51 +1,41 @@
 #include "../pch.h"
 #include "../plugin_64.h"
 
-extern "C" {
-	int add(int a, int b);
-    int addA(int a, int b);
-}
-
-int add(int a, int b)
-{
-  return a + b;
-}
-
-int addA(int a, int b)
-{
-  return a + b;
-}
-
 namespace MainText {
-
     extern "C" {
         void mainTextProc1();
-        uintptr_t mainTextProc1JmpAddress;
-        uintptr_t mainTextProc1ReturnAddress;
     }
+    extern "C" uintptr_t mainTextProc1ReturnAddress;
+    extern "C" uintptr_t mainTextProc1JmpAddress;
 
 	DllError mainTextProc1Injector(RunOptions options) {
 		DllError e = {};
 
 		switch (options.version) {
 		case v3_3_0:
-			// mov rdx, [r12 + rcx*8 + 0D0h]
-			BytePattern::temp_instance().find_pattern("49 8B 94 CC D0 00 00 00");
-			if (BytePattern::temp_instance().has_size(1, u8"テキスト処理ループ１の文字取得修正")) {
-				uintptr_t address = BytePattern::temp_instance().get_first().address();
+            // mov rdx, [r12 + rcx*8 + 0D0h]
+            BytePattern::temp_instance().find_pattern("49 8B 94 CC D0 00 00 00");
+            if (BytePattern::temp_instance().has_size(1, u8"テキスト処理ループ１のFont取得先")) {
+                mainTextProc1JmpAddress = BytePattern::temp_instance().get_first().address();
+            }
+            else {
+                e.unmatch.mainTextProc1Injector = true;
+            }
 
-                // jz      short loc_xxxxx
-				mainTextProc1JmpAddress = address + 0x69;
+            // movsxd rax, ebp
+            BytePattern::temp_instance().find_pattern("48 63 C5 0F B6 88 00 8F 41 02");
+            if (BytePattern::temp_instance().has_size(1, u8"テキスト処理ループ１の文字取得先")) {
+                uintptr_t address = BytePattern::temp_instance().get_first().address();
 
-                // mov esi, [rdx+18h]
-                mainTextProc1ReturnAddress = address + 0x10;
+                // mov     ds:src[rdx], cl
+                mainTextProc1ReturnAddress = address + 0x11;
 
-				Injector::MakeJMP(address, mainTextProc1, true);
+                Injector::MakeJMP(address,mainTextProc1,true);
 			}
 			else {
 				e.unmatch.mainTextProc1Injector = true;
 			}
-			break;
+            break;
 		default:
 			e.version.mainTextProc1Injector = true;
 		}
@@ -55,6 +45,7 @@ namespace MainText {
 
 	/*-----------------------------------------------*/
 
+    // CBitmapFont::RenderToScreen
 	DllError Init(RunOptions options) {
 		DllError result = {};
 
--- hook/main_text/main_text_asm.asm
+++ hook/main_text/main_text_asm.asm
@@ -1,12 +1,79 @@
+%define ESCAPE_SEQ_1    0x10
+%define ESCAPE_SEQ_2    0x11
+%define ESCAPE_SEQ_3    0x12
+%define ESCAPE_SEQ_4    0x13
+%define LOW_SHIFT       0x0F
+%define HIGH_SHIFT      0x09
+%define SHIFT_2		LOW_SHIFT
+%define SHIFT_3		0x900
+%define SHIFT_4         0x8F1
+%define NO_FONT         0x98F
+%define NOT_DEF         0x2026
+
 GLOBAL mainTextProc1
-EXTERN addA
+GLOBAL mainTextProc1ReturnAddress
+GLOBAL mainTextProc1JmpAddress
+
+[SECTION .data]
+mainTextProc1ReturnAddress: DQ 0
+mainTextProc1JmpAddress: DQ 0
 
 [SECTION .text]
 mainTextProc1:
-    mov     rdx, QWORD [r12 + rcx*8 + 0xD0]
-    test    rdx, rdx
-    jz      1
-    call    addA
-    mov     ecx, DWORD [rdx + 0x0C]
-    push    1
+    movsxd  rax, ebp
+    movzx   ecx, BYTE [rax+0x2418f00]
+    movsxd  rdx, ebx
+    lea     r14, [rdx+1]
+
+    push QWORD [rel mainTextProc1ReturnAddress]
     ret
+
+mainTextProc2:
+    cmp     BYTE [r12 + rcx], ESCAPE_SEQ_1
+    jz      .JMP_A
+    cmp     BYTE [r12 + rcx], ESCAPE_SEQ_2
+    jz      .JMP_B
+    cmp     BYTE [r12 + rcx], ESCAPE_SEQ_3
+    jz      .JMP_C
+    cmp     BYTE [r12 + rcx], ESCAPE_SEQ_4
+    jz      .JMP_D
+    movzx   edx, BYTE [r12 + rcx]
+    jmp     .JMP_E
+
+.JMP_A:
+    movzx   edx, WORD [r12 + rcx + 1]
+    jmp     .JMP_F
+
+.JMP_B:
+    movzx   edx, WORD [r12 + rcx + 1]
+    sub     edx, SHIFT_2
+    jmp     .JMP_F
+
+.JMP_C:
+    movzx   edx, WORD [r12 + rcx + 1]
+    add     edx, SHIFT_3
+    jmp     .JMP_F
+
+.JMP_D:
+    movzx   edx, WORD [r12 + rcx + 1]
+    add     edx, SHIFT_4
+
+.JMP_F:
+    movzx   edx, dx
+    add     ebx, 2
+    cmp     edx, NO_FONT
+
+    ja      .JMP_E
+    mov     edx, NOT_DEF
+
+.JMP_E:
+    mov     rdx, [r12 + rcx * 8 + 0x0D0]
+
+    test    rdx, rdx
+
+    jz      mainTextProc1JmpAddress wrt ..plt
+    mov     ecx, DWORD [rdx + 0x0C]
+
+    mov     rsi, mainTextProc1ReturnAddress
+    mov     rsi, [rsi]
+    jmp     rsi
